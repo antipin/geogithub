67a76a718278cb5c2bdc44090a9b5b1c1e1d8d41
Alexander Antipin <alex@antipin.com>
2017-10-21 06:34:07 +0300

    Add valid components for each app mode

--- src/components/root/root.js
+++ src/components/root/root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { BaseLayout, Mapbox, RepoPicker, RepoFetcher } from '../'
+import { BaseLayout, ErrorBox, Mapbox, RepoPicker, RepoFetcher, TryAgain } from '../'
 
 class Root extends Component {
 
@@ -11,24 +11,15 @@ class Root extends Component {
         return (
             <BaseLayout>
                 <Mapbox/>
-                { Root.modeToComponent(mode) }
+                { this.modeToComponent(mode) }
             </BaseLayout>
         )
 
     }
 
-    static modeToComponent(mode) {
-    
-        /*
-            Application can be in the following modes:
-                1. waiting_for_repo_selection
-                2. repo_is_selected (attrs: repo_name)
-                3. fetching_repo_dataset (attrs: progress)
-                    3.1. fetching_repo_dataset_failed (attrs: error)
-                    3.2. fetching_repo_dataset_succeded (attrs: dataset)
-                4. visualising_repo_dataset
-                5. visualisation_completed
-        */
+    modeToComponent(mode) {
+
+        const { errorMessage } = this.props
         
         switch (mode) {
     
@@ -38,17 +29,21 @@ class Root extends Component {
     
             case 'repo_is_selected':
             case 'fetching_repo_dataset':
-                return <RepoFetcher/> // Progress bar
+                return <RepoFetcher/>
     
             case 'fetching_repo_dataset_failed':
-                return <RepoPicker/> // Error
+                return (
+                    <ErrorBox title="Looks like something went wrong with data fetching">
+                        { errorMessage }
+                    </ErrorBox>
+                )
     
             case 'fetching_repo_dataset_succeded':
             case 'visualising_repo_dataset':
-                return <RepoPicker/> // Visualizer
+                return null // Visualizer
     
             case 'visualisation_completed':
-                return <RepoPicker/> // Final screen
+                return <TryAgain/> // Final screen
     
         }
     
@@ -57,7 +52,8 @@ class Root extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    mode: state.mode
+    mode: state.mode,
+    errorMessage: state.error && state.error.message || null,
 })
 
 export default connect(mapStateToProps)(Root)
