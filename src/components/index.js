6a1d5a82c90bbf43210911239193dc098ef7540d
Alexander Antipin <alex@antipin.com>
2017-10-21 06:32:26 +0300

    Update component index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 export { default as Root } from './root'
 export { default as BaseLayout } from './base-layout'
+export { default as ErrorBox } from './error-box'
 export { default as Mapbox } from './mapbox'
 export { default as RepoPicker } from './repo-picker'
 export { default as RepoFetcher } from './repo-fetcher'
+export { default as TryAgain } from './try-again'
