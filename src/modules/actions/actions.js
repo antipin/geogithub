0fa82d5e77a88df056244347d488601dc8aba5e6
Alexander Antipin <alex@antipin.com>
2017-10-21 06:31:10 +0300

    Implement all core actions and reducers

--- src/modules/actions/actions.js
+++ src/modules/actions/actions.js
@@ -1,5 +1,59 @@
 import { createAction } from 'redux-actions'
+import { GeoGithubDataprovider } from '../'
+
+const sigmoidFunc = (x) => 1 / (1 + Math.pow(Math.E, -(x - 4)))
+
+const selectRepo = createAction('SELECT_REPO')
+const fetchRepoDatasetProgress = createAction('FETCH_REPO_DATASET_PROGRESS')
+const fetchRepoDatasetSucceeded = createAction('FETCH_REPO_DATASET_SUCCEEDED')
+const fetchRepoDatasetFailed = createAction('FETCH_REPO_DATASET_FAILED')
+const fetchRepoDataset = createAction('FETCH_REPO_DATASET', ({ repoPath, githubToken, mapboxToken }) =>
+    (dispatch) => {
+    
+        const geoGithubDataprovider = new GeoGithubDataprovider({ repoPath, githubToken, mapboxToken })
+        
+        // Fake progress
+        let progress = 0
+        const progressTimer = setInterval(
+            () => {
+
+                progress += 0.5
+
+                dispatch(fetchRepoDatasetProgress(
+                    sigmoidFunc(progress)
+                ))
+
+            },
+            500
+        )
+
+        geoGithubDataprovider.fetch()
+            .then(dataset => { 
+                
+                clearInterval(progressTimer)
+                dispatch(fetchRepoDatasetProgress(1))
+                
+                return Promise.resolve(dataset)
+
+            })
+            .then(dataset => dispatch(fetchRepoDatasetSucceeded(dataset)))
+            .catch(error => {
+                
+                clearInterval(progressTimer)
+                dispatch(fetchRepoDatasetFailed(error))
+
+            })
+    
+    })
+
+const startVisualisation = createAction('VISUALISATION_STARTED')
+const completeVisualisation = createAction('VISUALISATION_COMPLETED')
+const tryAgain = createAction('TRY_AGAIN')
 
 export default {
-    selectRepo: createAction('SELECT_REPO'),
+    selectRepo,
+    fetchRepoDataset,
+    startVisualisation,
+    completeVisualisation,
+    tryAgain,
 }
--- src/modules/reducer/reducer.js
+++ src/modules/reducer/reducer.js
@@ -1,25 +1,45 @@
-import keymirror from 'keymirror'
 import { handleActions } from 'redux-actions'
 
-const MODES = keymirror({
-    waiting_for_repo_selection: '',
-    repo_is_selected: '',
-})
-const INITIAL_STATE = {
-    mode: MODES.waiting_for_repo_selection,
-}
-
-function makeReducer() {
+function makeReducer({ initialState, modes }) {
 
     const handlers = {
         SELECT_REPO: (state, action) => ({
             ...state,
-            mode: MODES.repo_is_selected,
-            repo_name: action.payload
+            mode: modes.repo_is_selected,
+            repo_path: action.payload
+        }),
+        FETCH_REPO_DATASET: (state) => ({
+            ...state,
+            mode: modes.fetching_repo_dataset,
+        }),
+        FETCH_REPO_DATASET_PROGRESS: (state, action) => ({
+            ...state,
+            progress: action.payload,
+        }),
+        FETCH_REPO_DATASET_FAILED: (state, action) => ({
+            ...state,
+            mode: modes.fetching_repo_dataset_failed,
+            error: action.payload,
+        }),
+        FETCH_REPO_DATASET_SUCCEEDED: (state, action) => ({
+            ...state,
+            mode: modes.fetching_repo_dataset_succeded,
+            dataset: action.payload,
+        }),
+        VISUALISATION_STARTED: (state) => ({
+            ...state,
+            mode: modes.visualising_repo_dataset,
+        }),
+        VISUALISATION_COMPLETED: (state) => ({
+            ...state,
+            mode: modes.visualisation_completed,
+        }),
+        TRY_AGAIN: () => ({
+            ...initialState,
         }),
     }    
 
-    return handleActions(handlers, INITIAL_STATE)
+    return handleActions(handlers, initialState)
 
 }
 
